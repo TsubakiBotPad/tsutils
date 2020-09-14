bfe171d1bd6883a5d9c174a93a41555d0a365bec
AA <chasehult@gmail.com>
2020-09-14 00:47:43 -0700

    Package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tsutils",
+    version="1.0.0",
+    author="The Tsubotki Team",
+    author_email="69992611+TsubakiBotPad@users.noreply.github.com",
+    license="MIT",
+    description="A collection of helper commands for Red-DiscordBot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TsubakiBotPad/tsutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
