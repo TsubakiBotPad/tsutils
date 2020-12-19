488c7d0fb4fdfe2d20be6c836706e99c785f32cc
AA <chasehult@gmail.com>
2020-12-19 02:25:43 -0800

    Add is_donor

--- tsutils/tsutils.py
+++ tsutils/tsutils.py
@@ -635,3 +635,14 @@ def auth_check(perm, default=False):
             return default
         return authcog.settings.get_perm(ctx.author.id, perm, default=default)
     return commands.check(check)
+
+
+def is_donor(only_patron=False):
+    def check(ctx):
+        if ctx.author.id in ctx.bot.owner_ids:
+            return True
+        DCOG = ctx.bot.get_cog("Donations")
+        if not DCOG:
+            return False
+        return DCOG.is_donor(ctx, only_patron)
+    return commands.check(check)
