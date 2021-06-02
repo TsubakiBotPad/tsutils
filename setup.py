0295c477dd22b15e805b4a47a34419db23ada0ba
AA <chasehult@gmail.com>
2021-06-01 21:30:46 -0700

    Copy fm_flags (#30)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsutils",
-    version="3.3.5",
+    version="3.3.6",
     author="The Tsubotki Team",
     author_email="69992611+TsubakiBotPad@users.noreply.github.com",
     license="MIT",
