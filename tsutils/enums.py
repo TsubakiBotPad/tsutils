a0e06148228f88dbcabf852039fe9d716af93923
AA <chasehult@gmail.com>
2021-06-07 05:11:44 -0700

    Use a safer enum object

--- tsutils/enums.py
+++ tsutils/enums.py
@@ -1,23 +1,32 @@
 from enum import Enum
 
-class Server(Enum):
+
+class SmartEqEnum(Enum):
+    def __eq__(self, other):
+        if isinstance(other, Enum):
+            return self.name == other.name \
+                   and self.value == other.value \
+                   and self.__class__.__name__ == other.__class__.__name__
+        return False
+
+class Server(SmartEqEnum):
     COMBINED = "COMBINED"
     JP = "JP"
     NA = "NA"
     KR = "KR"
 
 
-class EvoToFocus(Enum):
+class EvoToFocus(SmartEqEnum):
     newest = 0
     naprio = 1
 
 
-class AltEvoSort(Enum):
+class AltEvoSort(SmartEqEnum):
     dfs = 0
     numerical = 1
 
 
-class ChildMenuSelector(Enum):
+class ChildMenuSelector(SmartEqEnum):
     IdMenu = 0
     NaDiffMenu = 1
     AwakeningList = 2
