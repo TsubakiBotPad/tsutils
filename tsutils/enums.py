1371bb377eb48b868f614639ded621a8f30a13b1
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-06-10 01:32:58 -0500

    Add LsMultiplier & CardPlusModifier (#37)

--- tsutils/enums.py
+++ tsutils/enums.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class Server(Enum):
     COMBINED = "COMBINED"
     JP = "JP"
@@ -21,3 +22,13 @@ class ChildMenuSelector(Enum):
     IdMenu = 0
     NaDiffMenu = 1
     AwakeningList = 2
+
+
+class LsMultiplier(Enum):
+    lsdouble = 0
+    lssingle = 1
+
+
+class CardPlusModifier(Enum):
+    plus0 = 0
+    plus297 = 1
