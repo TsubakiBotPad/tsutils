cf067d5c548312edd7e48d0bd652ef033b83eb4f
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-07-25 17:39:17 -0500

    Support menu emojis provided as tuples with fallbacks (#42)

--- tsutils/menu/panes.py
+++ tsutils/menu/panes.py
@@ -11,11 +11,40 @@ class MenuPanes:
 
     @classmethod
     def emoji_names(cls):
+        """Return all emoji names. If an emoji is specified as a tuple with fallback(s), returns a tuple."""
         return [k for k, v in cls.DATA.items() if k not in cls.HIDDEN_EMOJIS]
 
     @classmethod
+    def all_emoji_names(cls):
+        """
+        Return all valid emoji names, including fallbacks.
+        In particular, the length of the list returned by this method is not guaranteed to be
+        the same as the length of the list returned by `emoji_names`, and so this should only
+        be used for validity checking.
+        """
+        ret = []
+        for k, v in cls.DATA.items():
+            if k in cls.HIDDEN_EMOJIS:
+                continue
+            if isinstance(k, str):
+                ret.append(k)
+            elif isinstance(k, tuple):
+                # case when a fallback is provided
+                ret += k
+        return ret
+
+    @classmethod
     def transitions(cls):
-        return {k: v[0] for k, v in cls.DATA.items() if v[0] is not None}
+        ret = {}
+        for k, v in cls.DATA.items():
+            if v[0] is None:
+                continue
+            if isinstance(k, str):
+                ret[k] = v[0]
+            elif isinstance(k, tuple):
+                # case when a fallback is provided
+                ret.update({_: v[0] for _ in k})
+        return ret
 
     @classmethod
     def pane_types(cls):
