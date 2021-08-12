42135ee52955570277ec6ff04a83dd261410d391
AA <chasehult@gmail.com>
2021-08-12 01:47:55 -0700

    Add UserFeedbackException

--- tsutils/errors.py
+++ tsutils/errors.py
@@ -1,11 +1,21 @@
-from discord.ext.commands import CommandError
+from redbot.core.commands import UserFeedbackCheckFailure
 
 
 class IndexNotLoaded(KeyError):
     pass
 
 
-class NoAPIKeyException(CommandError):
+class UserFeedbackException(UserFeedbackCheckFailure):  # TODO: Find another way to do this
+    """An error that sends its message to the user instead of failing a command
+
+    This error has nothing to do with checks, and only inherits from UserFeedbackCheckFailure
+    because the Red bot instance automatically sends its message to the current
+    channel when raised.
+    """
+    pass
+
+
+class NoAPIKeyException(UserFeedbackException):
     def __init__(self, fix_command=None, *args):
         if fix_command is not None:
             m = f"API keys not found.  Use `{fix_command}` to set them."
