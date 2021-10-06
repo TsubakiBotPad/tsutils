52149b25228e763e7a9c8e956d5dfa8814d19abe
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-10-06 10:49:42 -0500

    Properly default cardlv (#73)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsutils",
-    version="5.2.1",
+    version="5.2.2",
     author="The Tsubotki Team",
     author_email="69992611+TsubakiBotPad@users.noreply.github.com",
     license="MIT",
