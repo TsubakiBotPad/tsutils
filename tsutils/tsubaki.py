d2a71f10ce84631736ed91b39bca26ff2ef06305
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-01-10 12:46:16 -0600

    add number_emoji_small

--- tsutils/tsubaki.py
+++ tsutils/tsubaki.py
@@ -1,1 +1,7 @@
+from discordmenu.emoji.emoji_cache import emoji_cache
+
 CLOUDFRONT_URL = "https://d30r6ivozz8w2a.cloudfront.net"
+
+
+def number_emoji_small(num: int):
+    return emoji_cache.get_emoji(f"bullet_{num}")
