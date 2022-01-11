547e3bc03ef03d3253bf210e8753a7f781d52db4
AA <chasehult@gmail.com>
2022-01-10 21:18:39 -0800

    default kwarg

--- tsutils/tsubaki.py
+++ tsutils/tsubaki.py
@@ -6,4 +6,4 @@ CLOUDFRONT_URL = "https://d30r6ivozz8w2a.cloudfront.net"
 
 
 def number_emoji_small(num: int):
-    return emoji_cache.get_emoji(f"bullet_{num}", char_to_emoji(num))
+    return emoji_cache.get_emoji(f"bullet_{num}", default=char_to_emoji(num))
