a4062693140998ce0d5482dc4144e73624568286
River <18037011+RheingoldRiver@users.noreply.github.com>
2022-02-23 05:27:47 -0600

    Add padindex, ilmina support (#82)

--- tsutils/tsubaki.py
+++ tsutils/tsubaki.py
@@ -1,4 +1,8 @@
+import urllib.parse
+
 from discordmenu.emoji.emoji_cache import emoji_cache
+
+from tsutils.pad import get_pdx_id
 
 CLOUDFRONT_URL = "https://d30r6ivozz8w2a.cloudfront.net"
 MEDIA_PATH = CLOUDFRONT_URL + '/media/'
@@ -8,6 +12,12 @@ VIDEO_TEMPLATE = MEDIA_PATH + 'animated_portraits/{0:05d}.mp4'
 GIF_TEMPLATE = MEDIA_PATH + 'animated_portraits/{0:05d}.gif'
 ORB_SKIN_TEMPLATE = MEDIA_PATH + 'orb_skins/jp/{0:03d}.png'
 ORB_SKIN_CB_TEMPLATE = MEDIA_PATH + 'orb_skins/jp/{0:03d}cb.png'
+
+INFO_PDX_TEMPLATE = 'http://www.puzzledragonx.com/en/monster.asp?n={}'
+YT_SEARCH_TEMPLATE = 'https://www.youtube.com/results?search_query={}'
+SKYOZORA_TEMPLATE = 'http://pad.skyozora.com/pets/{}'
+ILMINA_TEMPLATE = 'https://ilmina.com/#/CARD/{}'
+PADINDEX_TEMPLATE = 'https://pad.chesterip.cc/'
 
 
 def number_emoji_small(num: int):
@@ -38,3 +48,29 @@ class MonsterImage:
     @staticmethod
     def orb_skin_colorblind(orb_skin_id: int):
         return ORB_SKIN_CB_TEMPLATE.format(orb_skin_id)
+
+
+class MonsterLink:
+    @staticmethod
+    def puzzledragonx(m):
+        return INFO_PDX_TEMPLATE.format(get_pdx_id(m))
+
+    @staticmethod
+    def youtube_search(m):
+        return YT_SEARCH_TEMPLATE.format(urllib.parse.quote(m.name_ja))
+
+    @staticmethod
+    def skyozora(m):
+        return SKYOZORA_TEMPLATE.format(m.monster_no_jp)
+
+    @staticmethod
+    def ilmina(m):
+        return ILMINA_TEMPLATE.format(m.monster_no_jp)
+
+    @staticmethod
+    def ilmina_skill(m):
+        return "https://ilmina.com/#/SKILL/{}".format(m.active_skill.active_skill_id) if m.active_skill else None
+
+    @staticmethod
+    def padindex(m):
+        return PADINDEX_TEMPLATE.format(m.monster_no_jp)
