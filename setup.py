5306d7c8fc8d7a9fdc568f6041c35f215b5613db
AA <chasehult@gmail.com>
2022-07-20 19:17:54 -0700

    Add new awakenings (#107)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsutils",
-    version="7.2.8",
+    version="7.2.9",
     author="The Tsubotki Team",
     author_email="69992611+TsubakiBotPad@users.noreply.github.com",
     license="MIT",
