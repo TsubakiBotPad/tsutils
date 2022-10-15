8911f5be3d6559e626405fcd3d62288d34bf1e01
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-10-15 17:21:20 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsutils",
-    version="7.2.11",
+    version="7.2.12",
     author="The Tsubotki Team",
     author_email="69992611+TsubakiBotPad@users.noreply.github.com",
     license="MIT",
