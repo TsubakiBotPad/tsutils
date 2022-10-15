3a502c7059627b3df404a6b32fb62200c9514443
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-10-15 17:04:25 -0500

    Alphabetize enums

--- tsutils/query_settings/enums.py
+++ tsutils/query_settings/enums.py
@@ -1,45 +1,9 @@
 from enum import Enum
 
 
-class EvoToFocus(Enum):
-    newest = 0
-    naprio = 1
-
-
-class OrModifierPriority(Enum):
-    orfirst = True
-    orlast = False
-
-
 class AltEvoSort(Enum):
     dfs = 0
     numerical = 1
-
-
-class ChildMenuType(Enum):
-    IdMenu = 0
-    NaDiffMenu = 1
-    AwakeningList = 2
-
-
-class LsMultiplier(Enum):
-    lsdouble = 0
-    lssingle = 1
-
-
-class CardPlusModifier(Enum):
-    plus0 = 0
-    plus297 = 1
-
-
-class EvoGrouping(Enum):
-    splitevos = 0
-    groupevos = 1
-
-
-class CardModeModifier(Enum):
-    solo = 0
-    coop = 1
 
 
 class CardLevelModifier(Enum):
@@ -48,9 +12,45 @@ class CardLevelModifier(Enum):
     lv120 = 2
 
 
+class CardModeModifier(Enum):
+    solo = 0
+    coop = 1
+
+
+class CardPlusModifier(Enum):
+    plus0 = 0
+    plus297 = 1
+
+
+class ChildMenuType(Enum):
+    IdMenu = 0
+    NaDiffMenu = 1
+    AwakeningList = 2
+
+
+class EvoGrouping(Enum):
+    splitevos = 0
+    groupevos = 1
+
+
+class EvoToFocus(Enum):
+    newest = 0
+    naprio = 1
+
+
+class LsMultiplier(Enum):
+    lsdouble = 0
+    lssingle = 1
+
+
 class MonsterLinkTarget(Enum):
     padindex = 0
     ilmina = 1
+
+
+class OrModifierPriority(Enum):
+    orfirst = True
+    orlast = False
 
 
 class ShowHelp(Enum):
