c899bc2df4b396df6625972d709c043df56b17d1
AA <chasehult@gmail.com>
2023-01-03 15:24:34 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsutils",
-    version="7.2.18",
+    version="7.2.19",
     author="The Tsubotki Team",
     author_email="69992611+TsubakiBotPad@users.noreply.github.com",
     license="MIT",
